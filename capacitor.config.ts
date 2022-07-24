41211012ed1964a48d027bddf0d053746113626f
SeanAnderson2022 <108705392+SeanAnderson2022@users.noreply.github.com>
2022-07-24 22:12:41 +0800

    Add files via upload
    This is the first build of the project. All of the features are taken from the main ionic website

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'notif mute system',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
